bf029e17a7317f8211268a3cabc498e587086ef6
Desta Fajri Dwi Purnomo <106084465+destafajri@users.noreply.github.com>
2023-10-29 04:17:35 +0700

    Update JavaApplication.java

--- src/main/java/com/backend/java/JavaApplication.java
+++ src/main/java/com/backend/java/JavaApplication.java
@@ -9,13 +9,6 @@ import java.util.Collections;
 public class JavaApplication {
 
 	public static void main(String[] args) {
-		// Add a delay of 30 seconds (30000 milliseconds)
-	        try {
-	            Thread.sleep(30000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-
 		SpringApplication app = new SpringApplication(JavaApplication.class);
 		String port = System.getenv("PORT");
 		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
